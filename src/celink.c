6a09316a8f011be73a97dc215e83a8e7b81da3fc
Peltlas <peltlasvulpine@gmail.com>
2026-08-15 14:03:58 +0800

    Add CELinK core stubs

---
+++ src/celink.c
@@ -0,0 +1,11 @@
+#include "celink.h"
+
+void celink_init(void)
+{
+    /* CELinK initialization will go here later. */
+}
+
+void celink_disconnect(void)
+{
+    /* CELinK cleanup will go here later. */
+}
